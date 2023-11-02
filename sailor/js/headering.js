ff5a95014cae8a24ef68d8ff668f45b92a2f0363
RJ3008 <120433730+RJ3008@users.noreply.github.com>
2023-11-02 06:51:19 -0400

    Create headering.js

---
+++ sailor/js/headering.js
@@ -0,0 +1,17 @@
+const header = document.getElementById('headerad');
+const hedercode = `
+<header class="masddddd">
+  <h1 style="color: white; margin-left: 20px; float: left; padding-bottom: 1px; padding-top: 6px;"><a id="fjds" href="http://71.193.100.230/">SailorOS</a></h1>
+  <nav>
+    <ul>
+      <li><a class="sadnasdf" href="http://71.193.100.230/Products/products.html">Products</a></li>
+      <li><a class="sadnasdf" href="#">Docs</a></li>
+      <li><a class="sadnasdf" href="#">Community</a></li>
+      <li><a class="sadnasdf" href="#">About</a></li>
+      <li><a class="sadnasdf" href="#">Legal</a></li>
+    </ul>
+  </nav>
+</header>
+`;
+
+header.innerHTML = hedercode;
